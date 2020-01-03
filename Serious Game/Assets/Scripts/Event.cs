743077983cbcdec9e5a17cabc1934ba92745027c
YanaOnushkina <y.g.onushkina@student.tue.nl>
2020-01-03 12:25:03 +0100

    Complete menu

---
+++ Serious Game/Assets/Scripts/Event.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event : MonoBehaviour
+{
+    public GameObject panel;
+    public void Deactivate()
+    {
+        panel.SetActive(false);
+    }
+}
---
+++ Serious Game/Assets/Scripts/GoToBuilding.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToBuilding : MonoBehaviour
+{
+    public GameObject button;
+    public GameObject text;
+
+    public void Building()
+    {
+        if (button.name == "Button Metaforum")
+        {
+            Debug.Log("Metaforum");
+            SceneManager.LoadScene("Metaforum");
+        }
+
+        if (button.name == "Button Atlas")
+        {
+            Debug.Log("Atlas");
+            SceneManager.LoadScene("Atlas");
+        }
+
+        if (button.name == "Button Luna")
+        {
+            Debug.Log("Luna");
+            text.SetActive(true);
+        }
+
+        if (button.name == "Button Helix")
+        {
+            Debug.Log("Helix");
+            text.SetActive(true);
+        }
+
+        if (button.name == "Button Aurora")
+        {
+            Debug.Log("Aurora");
+            text.SetActive(true);
+        }
+
+        if (button.name == "Button LaPlace")
+        {
+            Debug.Log("LaPlace");
+            text.SetActive(true);
+        }
+
+        if (button.name == "Button Gemini South")
+        {
+            Debug.Log("Gemini South");
+            text.SetActive(true);
+        }
+
+        if (button.name == "Button Gemini North")
+        {
+            Debug.Log("Gemini North");
+            text.SetActive(true);
+        }
+
+        if (button.name == "Button Flux")
+        {
+            Debug.Log("Flux");
+            text.SetActive(true);
+        }
+
+        if (button.name == "Button Traverse")
+        {
+            Debug.Log("Traverse");
+            text.SetActive(true);
+        }
+
+        if (button.name == "Button Sport Centrum")
+        {
+            Debug.Log("Sport Centrum");
+            text.SetActive(true);
+        }
+
+        if (button.name == "Button Auditorium")
+        {
+            Debug.Log("Auditorium");
+            text.SetActive(true);
+        }
+    }
+
+
+}
